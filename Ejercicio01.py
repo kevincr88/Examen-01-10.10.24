08d704e285f702c99b351a909fe2ca9ce566eaee
kevincr88 <komongele@est.utn.ac.cr>
2024-10-10 18:14:42 -0600

    Parte final del ejercicio 01

--- Ejercicio01.py
+++ Ejercicio01.py
@@ -29,6 +29,18 @@ def FullOuterJoin():
   for act in vec1:       #esto es para los elementos que estan en el vec1 pero no en el vec2
    if act not in vec2:
     salida.append(act)
+   return salida
+
+#------------------------------
+#Right Join where a.key is null
+#------------------------------
+
+def RighJoin():
+ salida= []
+ for act in vec2:  #Esta funcion es para imprimir los elementos que estan solo en el Vec2 pero no en el Vec1
+  if act not in vec1:
+   salida.append(act)
+  return salida
 
 # -----------------------------------
 # Executing joins functions
@@ -39,3 +51,4 @@ print(FullJoin())
 print()
 print("Funcion para imprimir elementos presentes en el Vec1, pero no presentes en el Vec2", FullOuterJoin())
 print()
+print ("Funcion para imprimir los elementos presentes unicamente en el Vec2, pero no en el Vec1", RighJoin())

8423c33bea0e6f07f31a6b5eb8507e5988dd0bbf
kevincr88 <komongele@est.utn.ac.cr>
2024-10-10 18:05:19 -0600

    Primera funcion del ejercicio 01

--- Ejercicio01.py
+++ Ejercicio01.py
@@ -20,10 +20,22 @@ def FullJoin():
  if not control:
  salida2.append(act)
  return salida2
+
+#----------------------------------------------------
+#Full Outer Join where a.key is null and b.key is null
+#-----------------------------------------------------
+def FullOuterJoin():
+  salida = []
+  for act in vec1:       #esto es para los elementos que estan en el vec1 pero no en el vec2
+   if act not in vec2:
+    salida.append(act)
+
 # -----------------------------------
 # Executing joins functions
 # -----------------------------------
 print(Join())
 print('')
 print(FullJoin())
+print()
+print("Funcion para imprimir elementos presentes en el Vec1, pero no presentes en el Vec2", FullOuterJoin())
 print()
